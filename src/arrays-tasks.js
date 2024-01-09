081f9a5cc021b4a9f02e647b6b88c4badab4dca2
Andrey Mironov <a.mironov@lucky-team.pro>
2024-01-09 16:24:45 +0300

    feat: task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -376,8 +376,13 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((acc, item) => {
+    if (!item) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 }
 
 /**
